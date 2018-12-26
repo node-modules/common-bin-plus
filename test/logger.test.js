7fdbe88c70eea067f47f58634a1c058f34125111
TZ <atian25@qq.com>
2018-12-26 14:28:33 +0800

    f

--- test/logger.test.js
+++ test/logger.test.js
@@ -42,4 +42,36 @@ describe('test/logger.test.js', () => {
       .expect('code', 0)
       .end();
   });
+
+  it('should log debug with ENV multi', () => {
+    return coffee.fork(bin, [], { env: { DEBUG: 'common-bin,CLI' } })
+      // .debug()
+      .expect('stdout', /\[MyCommand\] hello debug level/)
+      .expect('stdout', /\[MyCommand\] hello info level/)
+      .expect('stdout', /\[MyCommand\] hello log level/)
+      .expect('stderr', /\[MyCommand\] hello warn level/)
+      .expect('stderr', /\[MyCommand\] Error: hello error level\s*\n.*at MyCommand.run/)
+      .expect('code', 0)
+      .end();
+  });
+
+  it('should log debug with ENV *', () => {
+    return coffee.fork(bin, [], { env: { DEBUG: '*' } })
+      // .debug()
+      .expect('stdout', /\[MyCommand\] hello debug level/)
+      .expect('stdout', /\[MyCommand\] hello info level/)
+      .expect('stdout', /\[MyCommand\] hello log level/)
+      .expect('stderr', /\[MyCommand\] hello warn level/)
+      .expect('stderr', /\[MyCommand\] Error: hello error level\s*\n.*at MyCommand.run/)
+      .expect('code', 0)
+      .end();
+  });
+
+  it('should not log debug', () => {
+    return coffee.fork(bin, [], { env: { DEBUG: 'CLI_TEST' } })
+      // .debug()
+      .notExpect('stdout', /\[MyCommand\] hello debug level/)
+      .expect('code', 0)
+      .end();
+  });
 });
