9ecdc225ae27a745330187edeb816c27678a4a05
wanghx <whx89768@alibaba-inc.com>
2018-12-29 17:55:10 +0800

    feat: add ts support

---
+++ index.d.ts
@@ -0,0 +1,59 @@
+import { Inquirer } from 'inquirer';
+import * as CommonBin from 'common-bin';
+
+declare namespace CommonBinPlus {
+  export interface Logger extends Console {
+    level: string;
+  }
+  
+  export interface OptionDesc {
+    type: string;
+    description: string;
+  }
+
+  export interface Options {
+    [key: string]: OptionDesc;
+    verbose: OptionDesc;
+  }
+}
+
+declare class CommonBinPlus<T extends CommonBin.Context = CommonBin.Context> extends CommonBin {
+  /**
+   * Prompt function that takes a "question" object or array of question objects, and returns an object with responses from the user.
+   *
+   * @param {Array|Object} questions - Options objects for one or more prompts to run.
+   * @return {Promise<Object>} Promise that returns an "answers" object with the user's responses.
+   * @see https://github.com/SBoudrias/Inquirer.js
+   */
+  prompt: Inquirer['prompt'];
+
+  /**
+   * built-in logger
+   * @see https://github.com/node-modules/zlogger
+   */
+  logger: CommonBinPlus.Logger;
+
+  /**
+   * provide common line args definition to yargs
+   * @return {Object} options
+   * @protected
+   */
+  protected initOptions(): CommonBinPlus.Options;
+
+  /**
+   * if you want to custom it, override `initContext`
+   * @private
+   */
+  readonly context: T;
+
+  /**
+   * custom your context, origin getter of context will be freeze, here is the only way to customize it.
+   *
+   * @param {Object} context - super's context
+   * @return {Object} context obj
+   * @protected
+   */
+  protected initContext(ctx: CommonBin.Context): T;
+}
+
+export = CommonBinPlus;
--- test/index.test.js
+++ test/index.test.js
@@ -24,4 +24,14 @@ describe('test/index.test.js', () => {
       .expect('code', 1)
       .end();
   });
+
+  it('should compile ts without error', () => {
+    return coffee.fork(
+      require.resolve('typescript/bin/tsc'),
+      [ '-p', path.resolve(__dirname, './fixtures/ts/tsconfig.json') ]
+    )
+      // .debug()
+      .expect('code', 0)
+      .end();
+  });
 });
